c1440c05ada80166544513c57760ea828e5ced35
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-06-10 14:24:53 -0300

    new Account test

---
+++ tests/atm/AccountTest.java
@@ -0,0 +1,49 @@
+package atm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidatePIN() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAvailableBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTotalBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCredit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDebit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAccountNumber() {
+		fail("Not yet implemented");
+	}
+
+}
