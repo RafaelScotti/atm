c64c9a1a9bdb520373a49188f5c764e9cb8ffdfc
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-06-13 12:17:06 -0300

    .

---
+++ src/atm/CreateTransaction.java
@@ -0,0 +1,46 @@
+package atm;
+
+import atm.hardware.Keypad;
+import atm.transaction.BalanceInquiry;
+import atm.transaction.Deposit;
+import atm.transaction.Transaction;
+import atm.transaction.Withdrawal;
+
+public class CreateTransaction {
+//	Transaction balance;
+//	Transaction deposit;
+//	Transaction withdraw;
+//	
+//	public CreateTransaction(int accNumber) {
+//		 balance = new Transaction(new BalanceInquiry(accNumber, bankDatabase));
+//		 deposit = new Transaction(new Deposit(accNumber, bankDatabase));
+//		 withdraw = new Transaction(new Withdrawal(accNumber, bankDatabase));
+//		
+//		ATMMenu.display();
+//		int mainMenuSelection = Keypad.getInstance().getInput(); //keypad.getInput();
+//
+//		switch (mainMenuSelection){
+//
+//		case 1: 
+//			balance.execute();
+//			break; 
+//		case 2: 
+//			withdraw.execute();
+//			break; 
+//		case 3:
+//			deposit.execute();
+//			break; 
+//		case 4: 
+//			System.out.println("\nExiting the system...");
+//			userExited = true;
+//			break;
+//		default: 
+//			System.out.println("\nYou did not enter a valid selection. Try again.");
+//			break;
+//		} 
+//	} 
+//		
+//	}
+//	
+	
+}
